293e99cbaddb056ed68c44a2f49060227cb00e4e
JicksonHD <jacques.ghajar@lau.edu>
2022-04-18 23:42:39 +0300

    Added some comments and onclick methods for page 4

--- Front_end/app/src/main/java/com/example/final_project_front_end/MainActivity4.java
+++ Front_end/app/src/main/java/com/example/final_project_front_end/MainActivity4.java
@@ -2,13 +2,16 @@ package com.example.final_project_front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    //Declaring Variables
     Button materials;
     Button tasks;
     Button services;
@@ -21,6 +24,7 @@ public class MainActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
+        //Initilizing variables
         materials = (Button) findViewById(R.id.materialsButton);
         tasks = (Button) findViewById(R.id.taskButton);
         services = (Button) findViewById(R.id.servicesButton);
@@ -28,23 +32,53 @@ public class MainActivity4 extends AppCompatActivity {
         hint = (TextView) findViewById(R.id.hinttextView);
         helmet_logo = (ImageView) findViewById(R.id.helmetLogo);
 
+        //Adding animation to materials button
         materials.setTranslationX(-1500);
         materials.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to tasks button
         tasks.setTranslationX(-1500);
         tasks.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to services button
         services.setTranslationX(-1500);
         services.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to equipments button
         equipments.setTranslationX(-1500);
         equipments.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to helmet image
         helmet_logo.setTranslationX(-1500);
         helmet_logo.animate().translationXBy(1500).setDuration(1500);
 
+    }
 
 
+    public void takeMeToMaterials(View view){
+        //Onclick method that takes us to the material page
 
+        Intent materials_page = new Intent(getApplicationContext(),MainActivity5.class);
+    }
+
+    public void takeMeToEquip(View view){
+        //Onclick method that takes us to the equipment page
+
+
+        // Intent equipment_page = new Intent(getApplicationContext(),);
+    }
+
+    public void takeMeToTasks(View view){
+        //Onclick method that takes us to the Tasks page
+
+
+        // Intent tasks_page = new Intent(getApplicationContext(),);
+    }
+
+    public void takeMeToServices(View view){
+        //Onclick method that takes us to the services page
+
+
+        //  Intent services_page = new Intent(getApplicationContext(),);
     }
 }
