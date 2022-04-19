e42bf06c8e3b412c931f3a5660a779ab689f274b
JicksonHD <jacques.ghajar@lau.edu>
2022-04-19 19:35:17 +0300

    Animation added

--- Front_end/app/src/main/java/com/example/final_project_front_end/MainActivity5.java
+++ Front_end/app/src/main/java/com/example/final_project_front_end/MainActivity5.java
@@ -6,16 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity5 extends AppCompatActivity {
 
+
     EditText task_input;
     ListView task_list;
+    TextView removeHint;
+    Button addTask;
     ArrayList<String> theTask_list;
     ArrayAdapter<String> adapter;
 
@@ -26,6 +31,8 @@ public class MainActivity5 extends AppCompatActivity {
 
         task_input = (EditText) findViewById(R.id.task_input);
         task_list = (ListView) findViewById(R.id.task_list);
+        removeHint = (TextView) findViewById(R.id.removeHint);
+        addTask = (Button) findViewById(R.id.addTask);
         theTask_list = new ArrayList<String>();
         adapter = new ArrayAdapter<String >(getApplicationContext(), android.R.layout.simple_list_item_1,theTask_list);
 
@@ -39,6 +46,18 @@ public class MainActivity5 extends AppCompatActivity {
                 task_list.setAdapter(adapter);
             }
         });
+
+
+        task_input.setTranslationX(-1500);
+        task_input.animate().translationXBy(1500).setDuration(1500);
+
+        addTask.setTranslationX(-1500);
+        addTask.animate().translationXBy(1500).setDuration(1500);
+
+        removeHint.setTranslationY(1500);
+        removeHint.animate().translationYBy(-1500).setDuration(1500);
+
+
 
     }
 
