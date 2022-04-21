a0d4fca62e852895b192ab416edab0e5e4b97155
JicksonHD <jacques.ghajar@lau.edu>
2022-04-21 23:07:09 +0300

    Page 8 design

--- Front_end/app/src/main/java/com/example/final_project_front_end/MainActivity7.java
+++ Front_end/app/src/main/java/com/example/final_project_front_end/MainActivity7.java
@@ -14,6 +14,8 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity7 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    //Declaring variables
+    TextView services_hint_spinner;
     Spinner services;
     TextView pick_services_hint;
     TextView services_result;
@@ -29,6 +31,7 @@ public class MainActivity7 extends AppCompatActivity implements AdapterView.OnIt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
 
+        //Initilizing variables
         services = (Spinner) findViewById(R.id.services_spinner);
         services_result = (TextView) findViewById(R.id.services_result);
         pick_services_hint = (TextView) findViewById(R.id.pick_services_hint);
@@ -37,40 +40,53 @@ public class MainActivity7 extends AppCompatActivity implements AdapterView.OnIt
         floor = (EditText) findViewById(R.id.floor);
         city = (EditText) findViewById(R.id.city);
         time = (EditText) findViewById(R.id.time);
-
-
+        services_hint_spinner = (TextView) findViewById(R.id.services_hint_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.services, android.R.layout.simple_spinner_item);
+
+        //Setting adapter as dropdown list
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        //Linking adapter with services spinner
         services.setAdapter(adapter);
         services.setOnItemSelectedListener(MainActivity7.this);
 
+        //Adding animation to spinner
         services.setTranslationX(-1500);
         services.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to street textBox
         street.setTranslationX(-1500);
         street.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to building textBox
         building.setTranslationX(-1500);
         building.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to floor textBox
         floor.setTranslationX(-1500);
         floor.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to city textBox
         city.setTranslationX(-1500);
         city.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to time textBox
         time.setTranslationX(-1500);
         time.animate().translationXBy(1500).setDuration(1500);
 
+        //Adding animation to services hint
         pick_services_hint.setTranslationX(-1500);
         pick_services_hint.animate().translationXBy(1500).setDuration(1500);
+
+        //Adding animation to textView hint above spinner
+        services_hint_spinner.setTranslationX(-1500);
+        services_hint_spinner.animate().translationXBy(1500).setDuration(1500);
 
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+        //method  when item is selected in the spinner
         if (i == 0){
             input_service = "Plumber";
         }
@@ -82,10 +98,12 @@ public class MainActivity7 extends AppCompatActivity implements AdapterView.OnIt
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+        //method when item is not selected in the spinner
 
     }
 
     public void order(View view){
+        //Onclick method of order button
 
         input_building = building.getText().toString();
         input_city = city.getText().toString();
