bda0fff045e7ecf4ccb573a1734f0c4d7debe101
JicksonHD <jacques.ghajar@lau.edu>
2022-05-04 23:58:17 +0300

    Added fetching in materials API (fetching quantity and computing it and added some design for buttons (rounded)

--- Back_end/materials.php
+++ Back_end/materials.php
@@ -22,12 +22,38 @@ if(isset($_POST['material_name']) && isset($_POST['material_type']) && isset($_P
     $type = validate($_POST['material_type']);
 
 
-    $query = $mysqli->prepare("INSERT INTO materials(material_name, material_type, material_quantity, location_street, location_building, location_city) VALUES (?, ?, ?, ?, ?, ?);");
-    $query->bind_param("ssisss", $name, $type, $quantity, $location_s, $location_b, $location_c);
-    $query->execute();
 
-    
+    $query = "SELECT quantity_available FROM quantities WHERE material_name= '" . $name . "'";
+    $result = mysqli_query($mysqli, $query);
+
+    if (mysqli_num_rows($result) != 0) {
+
+        $row = mysqli_fetch_assoc($result);
+        $quantity_available = $row['quantity_available'];
+
+        if($quantity_available > $quantity){
+            $query2 = $mysqli->prepare("INSERT INTO materials(material_name, material_type, material_quantity, location_street, location_building, location_city) VALUES (?, ?, ?, ?, ?, ?);");
+            $query2->bind_param("ssisss", $name, $type, $quantity, $location_s, $location_b, $location_c);
+            $query2->execute();
+
+            $remaining_quantity = $quantity_available - $quantity;
+
+            $query3 = $mysqli->prepare("UPDATE quantities SET quantity_available = ? where material_name = ?;");
+            $query3->bind_param("is", $remaining_quantity, $name);
+            $query3->execute();
+
+            $response[] = $row;
+            $json_respnse = json_encode($response);
+            echo $json_respnse;
+
+        } 
+        else{
+            echo "Not enough quantity";
+        }   
+    }
+
 }
+
 
 
 
